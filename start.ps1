e894963da23d7af2bc963d38bfd80f523fc1c1fc
YtilaerJawoly <crystalord@gmail.com>
2026-07-04 18:08:09 +0800

    correct

--- start.ps1
+++ start.ps1
@@ -4,7 +4,7 @@ Set-Location $scriptDir
 
 $wt = Get-Command wt.exe -ErrorAction SilentlyContinue
 if ($wt) {
-    Start-Process wt.exe -ArgumentList '-w','0','nt','-p','PowerShell','-d',$scriptDir,'powershell','-Command','node bin\ksm.js'
+    Start-Process wt.exe -ArgumentList '-w','0','nt','-p','PowerShell','-d',$scriptDir,'powershell','-NoProfile','-Command','node bin\ksm.js'
 } else {
     node bin\ksm.js
 }
